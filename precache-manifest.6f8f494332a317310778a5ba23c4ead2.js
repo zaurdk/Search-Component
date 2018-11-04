047790b3119ee083d4f3e568212d9a1122893366
zaurdk <zaurdk@gmail.com>
2018-11-04 23:15:28 +0500

    Updates

---
+++ precache-manifest.6f8f494332a317310778a5ba23c4ead2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "50b2c8637aa25e7660ff",
+    "url": "/Search-Component/static/js/runtime~main.50b2c863.js"
+  },
+  {
+    "revision": "685c8f3eb005695ab424",
+    "url": "/Search-Component/static/js/main.685c8f3e.chunk.js"
+  },
+  {
+    "revision": "ec4b2b7820578a775a75",
+    "url": "/Search-Component/static/js/1.ec4b2b78.chunk.js"
+  },
+  {
+    "revision": "685c8f3eb005695ab424",
+    "url": "/Search-Component/static/css/main.80d9978e.chunk.css"
+  },
+  {
+    "revision": "ec4b2b7820578a775a75",
+    "url": "/Search-Component/static/css/1.5e907379.chunk.css"
+  },
+  {
+    "revision": "9ad43bcda9df7091e7e29a3c5bb78677",
+    "url": "/Search-Component/index.html"
+  }
+];

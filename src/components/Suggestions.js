7ba0849cabb6abb73c2f2ccd0aa6b9c97dd70f25
zaurdk <zaurdk@gmail.com>
2018-11-04 23:57:59 +0500

    unlimited suggestion list bug fix

--- src/components/Suggestions.js
+++ src/components/Suggestions.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-
+var keyId = 0;
 const Suggestions = ({ results: { suggestions }, suggestionSubmit, queryLenght }) => (
   Boolean(suggestions) && (
     <ul className="list-group" style={{'listStyleType': 'none', 'cursor': 'pointer'}}>
@@ -8,7 +8,7 @@ const Suggestions = ({ results: { suggestions }, suggestionSubmit, queryLenght }
         suggestions.map(({ value }) => (
           <li className="list-group-item mx-auto"
               style = {{'width': '50%'}}
-              key={value}
+              key={keyId++}
               onClick={() => suggestionSubmit(value)}>
                 <span style={{'color': 'white', 'background': 'cadetblue'}}>
                   {value.slice(0, queryLenght)}
